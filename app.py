6dba7c94ce758f54d0b8c0261732c2ca5ae7af71
kian8890 <41864431+kian8890@users.noreply.github.com>
2025-04-15 21:38:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -106,4 +106,4 @@ def api_check_response():
     return jsonify({'success': success, 'headers': headers_dict})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 
